f69d21c131f651ff9452fcdf5be014a6b0121de9
Kuan-Ting Kuo <r02229006@gmail.com>
2024-06-30 14:12:40 +0800

    Change 'filename' in ini_3d and z_coord to substitute the pattern for nudging cases

---
+++ RUN/ndg/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+if [ $(git symbolic-ref --short HEAD) != 'sp_ndg' ]; then
+    echo 'Git branch not at sp_ndg!'
+    exit
+fi
+
+for file in /data/W.eddie/GoAmazon/Congo_*_interp.txt; do
+    cd /data/W.eddie/VVM/RUN/ndg
+    filename=$(basename "$file")
+    temp="${filename#Congo_}"
+    datetime="${temp%_interp.txt}"
+
+    echo $datetime
+    if [ -e /data/W.eddie/VVM/DATA/${datetime} ];then
+        echo "$datetime exist! SKIP!"
+        continue
+    fi
+    sed -i "8s|\(set expname = \).*|\1$datetime|" vvm.setup
+    sed -i "s|\(set inic = \).*|\1${file}|" vvm.setup
+    sed -i "16s|\(set expname = \).*|\1$datetime|" vvm.run
+
+    ./vvm.setup
+    ./vvm.run
+    cd /data/W.eddie/VVM/DATA/${datetime}
+    sbatch run.sh
+done
