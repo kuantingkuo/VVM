fd084ebba4c00e810d7d6b6a2f392ba352da0d53
Kuan-Ting Kuo <r02229006@gmail.com>
2025-03-18 11:38:40 +0800

    update for using dyn from bubble cases

--- RUN/rst/run_test.sh
+++ RUN/rst/run_test.sh
@@ -7,13 +7,17 @@ fi
 
 inif=/data/W.eddie/VVM/CODE/ini_3d_module.F
 #for txt in /data/W.eddie/GoAmazon_VVM/inic.txt; do
-for txt in /data/W.eddie/GoAmazon_VVM/inic_const.txt; do #dry inic
-    for num in {195..195}; do
+#for txt in /data/W.eddie/GoAmazon_VVM/inic_const.txt; do #dry inic
+for txt in /data/W.eddie/GoAmazon_VVM/inic_lin58.txt; do #theta_e linearly decreases from level 5 to level 8
+#    for THs in {2..24..2}; do
+    for THs in 14; do
     cd /data/W.eddie/VVM/RUN/rst
-    frac=0.$num
-#    runname=tst
-    runname=test_${frac}circulation_const_WL6_08
-    sed -i "588,589s|\*.*$|\*${frac}|" $inif
+    num=06 # time=1min.
+    TH=$(printf "%02d" $THs)
+    frac=$(printf "%02d" $num)
+    runname=GoAmazon_line58_${TH}_t${frac}
+    dyn=/data/W.eddie/VVM/DATA/test_bubble_const_${TH}/archive/test_bubble_const_${TH}.L.Dynamic-0000${frac}.nc
+    sed -i "519s|\(dynamic_file = \).*$|\1\"${dyn}\"|" $inif
 
     if [ -e /data/W.eddie/VVM/DATA/${runname} ];then
         echo "/data/W.eddie/VVM/DATA/$runname exist! REMOVE DIR.!"
